7a0f9d738bdab11ee97e3b6c12a9008b2cc50b47
François Mentec <francois.mentec@hotmail.fr>
2017-11-08 22:40:51 +0100

    search form

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ var express = require('express')
 var app = express()
 var http = require('http')
 var server = http.Server(app)
+var bodyParser = require('body-parser')
 
 var port = 8080
 app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/html/index.html')
+})
+
+app.get('/summoner/*', (req, res) => {
+  res.sendFile(__dirname + '/public/html/summoner.html')
+})
 
 app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/public/html/index.html')
+  res.redirect('/')
+})
+
+app.post('/search', (req, res) => {
+  res.redirect('/summoner/' + req.body.server + '/' + req.body.summoner)
 })
 
 server.listen(port, () => {
